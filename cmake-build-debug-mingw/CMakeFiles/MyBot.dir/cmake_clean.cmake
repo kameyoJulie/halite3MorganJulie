fbfad3bf6edcfef43f93168813f0049ebc383030
morgan bucher <morgan124.bucher@gmail.com>
2020-01-18 16:52:14 +0100

    first commit

---
+++ cmake-build-debug-mingw/CMakeFiles/MyBot.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyBot.dir/MyBot.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/command.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/constants.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/dropoff.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/game.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/game_map.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/log.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/player.cpp.obj"
+  "CMakeFiles/MyBot.dir/hlt/ship.cpp.obj"
+  "MyBot.exe"
+  "MyBot.exe.manifest"
+  "MyBot.pdb"
+  "libMyBot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
