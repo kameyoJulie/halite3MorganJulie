dc697476f87434c6b0e2a4db65f9166bfe937708
Morgan <morgan124.bucher@gmail.com>
2020-02-18 15:31:14 +0100

    [TAK-2](refacto) remise en forme des ships

--- hlt/ship.cpp
+++ hlt/ship.cpp
@@ -1,6 +1,22 @@
 #include "ship.hpp"
 #include "input.hpp"
 
+bool hlt::Ship::is_full() const {
+    return halite >= constants::MAX_HALITE;
+}
+
+hlt::Command hlt::Ship::make_dropoff() const {
+    return hlt::command::transform_ship_into_dropoff_site(id);
+}
+
+hlt::Command hlt::Ship::move(hlt::Direction direction) const {
+    return hlt::command::move(id, direction);
+}
+
+hlt::Command hlt::Ship::stay_still() const {
+    return hlt::command::move(id, Direction::STILL);
+}
+
 std::shared_ptr<hlt::Ship> hlt::Ship::_generate(hlt::PlayerId player_id) {
     hlt::EntityId ship_id;
     int x;
--- hlt/ship.hpp
+++ hlt/ship.hpp
@@ -15,21 +15,11 @@ namespace hlt {
             halite(halite)
         {}
 
-        bool is_full() const {
-            return halite >= constants::MAX_HALITE;
-        }
+        bool is_full() const;
 
-        Command make_dropoff() const {
-            return hlt::command::transform_ship_into_dropoff_site(id);
-        }
-
-        Command move(Direction direction) const {
-            return hlt::command::move(id, direction);
-        }
-
-        Command stay_still() const {
-            return hlt::command::move(id, Direction::STILL);
-        }
+        Command make_dropoff() const;
+        Command move(Direction direction) const;
+        Command stay_still() const;
 
         static std::shared_ptr<Ship> _generate(PlayerId player_id);
     };
