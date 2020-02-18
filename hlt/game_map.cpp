3e0f42779c114f352f2b5156fc01cfea35779440
morgan bucher <morgan124.bucher@gmail.com>
2020-02-18 14:40:41 +0100

    [IA-1](refacto) mise en forme du code

--- hlt/game_map.cpp
+++ hlt/game_map.cpp
@@ -2,6 +2,81 @@
 #include "game_map.hpp"
 #include "input.hpp"
 
+hlt::MapCell *hlt::GameMap::at(const hlt::Position &position) {
+    Position normalized = normalize(position);
+    return &cells[normalized.y][normalized.x];
+}
+
+hlt::MapCell *hlt::GameMap::at(const hlt::Entity &entity) {
+    return at(entity.position);
+}
+
+hlt::MapCell *hlt::GameMap::at(const hlt::Entity *entity) {
+    return at(entity->position);
+}
+
+hlt::MapCell *hlt::GameMap::at(const shared_ptr<Entity> &entity) {
+    return at(entity->position);
+}
+
+int hlt::GameMap::calculate_distance(const hlt::Position &source, const hlt::Position &target) {
+    const auto& normalized_source = normalize(source);
+    const auto& normalized_target = normalize(target);
+
+    const int dx = std::abs(normalized_source.x - normalized_target.x);
+    const int dy = std::abs(normalized_source.y - normalized_target.y);
+
+    const int toroidal_dx = min(dx, width - dx);
+    const int toroidal_dy = min(dy, height - dy);
+
+    return toroidal_dx + toroidal_dy;
+}
+
+hlt::Position hlt::GameMap::normalize(const hlt::Position &position) {
+    const int x = ((position.x % width) + width) % width;
+    const int y = ((position.y % height) + height) % height;
+    return { x, y };
+}
+
+vector<hlt::Direction> hlt::GameMap::get_unsafe_moves(const hlt::Position &source, const hlt::Position &destination) {
+    const auto& normalized_source = normalize(source);
+    const auto& normalized_destination = normalize(destination);
+
+    const int dx = std::abs(normalized_source.x - normalized_destination.x);
+    const int dy = std::abs(normalized_source.y - normalized_destination.y);
+    const int wrapped_dx = width - dx;
+    const int wrapped_dy = height - dy;
+
+    std::vector<Direction> possible_moves;
+
+    if (normalized_source.x < normalized_destination.x) {
+        possible_moves.push_back(dx > wrapped_dx ? Direction::WEST : Direction::EAST);
+    } else if (normalized_source.x > normalized_destination.x) {
+        possible_moves.push_back(dx < wrapped_dx ? Direction::WEST : Direction::EAST);
+    }
+
+    if (normalized_source.y < normalized_destination.y) {
+        possible_moves.push_back(dy > wrapped_dy ? Direction::NORTH : Direction::SOUTH);
+    } else if (normalized_source.y > normalized_destination.y) {
+        possible_moves.push_back(dy < wrapped_dy ? Direction::NORTH : Direction::SOUTH);
+    }
+
+    return possible_moves;
+}
+
+hlt::Direction hlt::GameMap::naive_navigate(shared_ptr<Ship> ship, const hlt::Position &destination) {
+    // get_unsafe_moves normalizes for us
+    for (auto direction : get_unsafe_moves(ship->position, destination)) {
+        Position target_pos = ship->position.directional_offset(direction);
+        if (!at(target_pos)->is_occupied()) {
+            at(target_pos)->mark_unsafe(ship);
+            return direction;
+        }
+    }
+
+    return Direction::STILL;
+}
+
 void hlt::GameMap::_update() {
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
--- hlt/game_map.hpp
+++ hlt/game_map.hpp
@@ -6,85 +6,25 @@
 #include <vector>
 using namespace std;
 namespace hlt {
-    struct GameMap {
+    class GameMap {
+    private:
         int width;
         int height;
         std::vector<std::vector<MapCell>> cells;
 
-        MapCell* at(const Position& position) {
-            Position normalized = normalize(position);
-            return &cells[normalized.y][normalized.x];
-        }
+    public:
+        MapCell* at(const Position& position);
+        MapCell* at(const Entity& entity);
+        MapCell* at(const Entity* entity);
+        MapCell* at(const std::shared_ptr<Entity>& entity);
 
-        MapCell* at(const Entity& entity) {
-            return at(entity.position);
-        }
+        int calculate_distance(const Position& source, const Position& target);
 
-        MapCell* at(const Entity* entity) {
-            return at(entity->position);
-        }
+        Position normalize(const Position& position);
 
-        MapCell* at(const std::shared_ptr<Entity>& entity) {
-            return at(entity->position);
-        }
+        std::vector<Direction> get_unsafe_moves(const Position& source, const Position& destination);
 
-        int calculate_distance(const Position& source, const Position& target) {
-            const auto& normalized_source = normalize(source);
-            const auto& normalized_target = normalize(target);
-
-            const int dx = std::abs(normalized_source.x - normalized_target.x);
-            const int dy = std::abs(normalized_source.y - normalized_target.y);
-
-            const int toroidal_dx = min(dx, width - dx);
-            const int toroidal_dy = min(dy, height - dy);
-
-            return toroidal_dx + toroidal_dy;
-        }
-
-        Position normalize(const Position& position) {
-            const int x = ((position.x % width) + width) % width;
-            const int y = ((position.y % height) + height) % height;
-            return { x, y };
-        }
-
-        std::vector<Direction> get_unsafe_moves(const Position& source, const Position& destination) {
-            const auto& normalized_source = normalize(source);
-            const auto& normalized_destination = normalize(destination);
-
-            const int dx = std::abs(normalized_source.x - normalized_destination.x);
-            const int dy = std::abs(normalized_source.y - normalized_destination.y);
-            const int wrapped_dx = width - dx;
-            const int wrapped_dy = height - dy;
-
-            std::vector<Direction> possible_moves;
-
-            if (normalized_source.x < normalized_destination.x) {
-                possible_moves.push_back(dx > wrapped_dx ? Direction::WEST : Direction::EAST);
-            } else if (normalized_source.x > normalized_destination.x) {
-                possible_moves.push_back(dx < wrapped_dx ? Direction::WEST : Direction::EAST);
-            }
-
-            if (normalized_source.y < normalized_destination.y) {
-                possible_moves.push_back(dy > wrapped_dy ? Direction::NORTH : Direction::SOUTH);
-            } else if (normalized_source.y > normalized_destination.y) {
-                possible_moves.push_back(dy < wrapped_dy ? Direction::NORTH : Direction::SOUTH);
-            }
-
-            return possible_moves;
-        }
-
-        Direction naive_navigate(std::shared_ptr<Ship> ship, const Position& destination) {
-            // get_unsafe_moves normalizes for us
-            for (auto direction : get_unsafe_moves(ship->position, destination)) {
-                Position target_pos = ship->position.directional_offset(direction);
-                if (!at(target_pos)->is_occupied()) {
-                    at(target_pos)->mark_unsafe(ship);
-                    return direction;
-                }
-            }
-
-            return Direction::STILL;
-        }
+        Direction naive_navigate(std::shared_ptr<Ship> ship, const Position& destination);
 
         void _update();
         static std::unique_ptr<GameMap> _generate();
